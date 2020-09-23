234964b36ee5159974163b3b435e12a14e3be4e5
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-09-23 20:56:31 +0900

    Strong Parameters、set_commentを記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:edit, :update, :destroy]
   def index
     @comments = Comment.all
   end
@@ -8,33 +9,47 @@ class CommentsController < ApplicationController
   end
 
   def confirm
-    @comment = Comment.new(content: params[:comment][:content])
+    @comment = Comment.new(comment_params)
   end
 
   def create
-    @comment = Comment.new(content: params[:comment][:content])
-    if @comment.valid?
-      @comment.save
-      redirect_to comments_path
-    else
+    @comment = Comment.new(comment_params)
+    if params[:back]
       render :new
+    else
+      if @comment.valid?
+        @comment.save
+        redirect_to comments_path
+      else
+        render :new
+      end
     end
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
     @comment.destroy
     redirect_to comments_path
   end
 
   def edit
-    @comment = Comment.find(params[:id])
+    @comment
   end
 
   def update
+    if @comment.update(comment_params)
+      redirect_to comments_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def set_comment
     @comment = Comment.find(params[:id])
-    @comment.update(content: params[:comment][:content])
-    redirect_to comments_path
   end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,3 @@
 class Comment < ApplicationRecord
-  validates :content, presence: true, length: {maximum: 140}
+  validates :content, presence: true, length: {maximum: 1}
 end
